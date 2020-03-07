eb833550de12d35691dc0d8162d0bd3f91f5825f
Leonid <golani04@gmail.com>
2020-03-07 17:33:10 +0200

    changes to fields

--- backend/models/projects.py
+++ backend/models/projects.py
@@ -1,20 +1,26 @@
-from datetime import date, datetime
 from dataclasses import dataclass, field
+from datetime import date, datetime
 from typing import List
-
-from .util import ItemID
 
 
 @dataclass
 class Project:
-    id: ItemID
-    name: str
-    maintainer: ItemID
-    description: str = field(default="", metadata="Describe a project purpose and about in short.")
+    id: str
+    name: str = field(compare=False)
+    maintainer: str = field(compare=False)
+    description: str = field(
+        default="",
+        repr=False,
+        metadata="Describe a project purpose and about in short.",
+        compare=False,
+    )
     favorite: bool = field(default=False, metadata="Give a project preference.")
-    _created: date = field(default_factory=date.today)
-    _updated: datetime = field(default_factory=datetime.utcnow)
+    _created: date = field(default_factory=date.today, repr=False)
+    _updated: datetime = field(default_factory=datetime.utcnow, repr=False)
     # FK, that collect all items that is connected to project
-    tags: List[ItemID] = field(default_factory=list)
-    users: List[ItemID] = field(default_factory=list)
-    issues: List[ItemID] = field(default_factory=list)
+    tags: List[str] = field(default_factory=list, repr=False, init=False, compare=False)
+    users: List[str] = field(default_factory=list, repr=False, init=False, compare=False)
+    issues: List[str] = field(default_factory=list, repr=False, init=False, compare=False)
+
+    def __post_init__(self):
+        print(self)
