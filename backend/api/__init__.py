8445f16ca1ffbedc2409ab942300adbb730cee96
Leonid <golani04@gmail.com>
2020-03-08 18:00:36 +0200

    API init

---
+++ backend/api/__init__.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+bp = Blueprint("api", __name__)
+
+
+@bp.before_request
+def _before_req():
+    """Load current user"""
+
+
+@bp.after_request
+def _after_req(response):
+    """Update user activity timeout."""
+
+    return response
+
+
+from . import projects  # noqa
