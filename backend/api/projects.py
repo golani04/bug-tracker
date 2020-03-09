f48c865a17da09ffb96758cf749d3870388011a9
Leonid <golani04@gmail.com>
2020-03-09 21:33:03 +0200

    create api, projects' routes

---
+++ backend/api/projects.py
@@ -0,0 +1,29 @@
+from flask import jsonify
+
+from backend import db
+from backend.api import bp
+
+
+@bp.route("/projects", methods=["GET"])
+def get_projects():
+    return jsonify(db.get_projects()), 200
+
+
+@bp.route("/projects", methods=["POST"])
+def create_project():
+    return jsonify(), 201
+
+
+@bp.route("/projects/<string:project_id>", methods=["GET"])
+def get_project(project_id: str):
+    return jsonify(), 200
+
+
+@bp.route("/projects/<string:project_id>", methods=["PUT", "PATCH"])
+def update_project(project_id: str):
+    return jsonify(), 200
+
+
+@bp.route("/projects/<string:project_id>", methods=["DELETE"])
+def delete_project(project_id: str):
+    return jsonify(), 200
