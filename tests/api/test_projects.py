a3b0ed700010992aa390ab7685065e54016a93c1
Leonid <golani04@gmail.com>
2020-03-13 15:42:06 +0200

    test API `post`

--- tests/api/test_projects.py
+++ tests/api/test_projects.py
@@ -1,6 +1,5 @@
 import pytest
-
-from backend import database as db
+from backend.models.projects import Project
 
 
 _MAINTAINER_ID = "123456abcdefghijklmnopqrstuvwxyz"[::-1] * 2
@@ -17,7 +16,8 @@ def test_projects_get(app):
 
 
 @pytest.mark.api
-def test_create_prject(app):
+def test_create_project(app):
+    length = len(Project.get_all_projects())
     response = app.post(
         "/api/v0/projects",
         json={
@@ -26,6 +26,45 @@ def test_create_prject(app):
             "description": "Any description",
         },
     )
-
+    print(response.get_json())
     assert response.status_code == 201
-    assert 0
+
+    projects = Project.get_all_projects()
+    assert length + 1 == len(projects)
+
+    project_api = response.get_json()
+    assert any([True for project in projects if project["id"] == project_api["id"]])
+
+
+@pytest.mark.api
+@pytest.mark.parametrize(
+    "data, expected",
+    [
+        ({"json": {}}, "Received json is empty."),
+        (
+            {
+                "data": {
+                    "name": "Test through an API.",
+                    "maintainer": _MAINTAINER_ID,
+                    "description": "Any description",
+                }
+            },
+            "Provided data is not a json.",
+        ),
+        (
+            {"json": {"maintainer": _MAINTAINER_ID, "description": "Any description"}},
+            "Missing required key: name.",
+        ),
+        (
+            {"json": {"name": "Test through an API.", "description": "Any description"}},
+            "Missing required key: maintainer.",
+        ),
+        ({"json": {"description": "Any description"}}, "Missing required keys: maintainer, name."),
+    ],
+)
+def test_create_project_failed(data, expected, app):
+    response = app.post("/api/v0/projects", **data)
+    assert response.status_code == 400
+
+    error = response.get_json()
+    assert error["message"] == expected
