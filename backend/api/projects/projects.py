b51787dcddcb22716e9781174947824a739726dc
Leonid <golani04@gmail.com>
2020-03-20 22:55:30 +0200

    project save is failed return status code=500

--- backend/api/projects/projects.py
+++ backend/api/projects/projects.py
@@ -2,6 +2,7 @@ from typing import Dict
 from flask import jsonify
 
 from backend.api import bp
+from backend.api.errors import error_response
 from backend.api.util import check_requested_data, check_required_keys, check_item_exists
 
 from backend.models.projects import Project
@@ -17,7 +18,8 @@ def get_projects():
 @check_required_keys({"name", "maintainer"})
 def create_project(data: Dict):
     project = Project.create(**data)
-    project.save()
+    if project.save() is False:
+        return error_response(500)
 
     return jsonify(project.to_dict()), 201
 
