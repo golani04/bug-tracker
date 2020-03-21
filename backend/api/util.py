ee222d6fb089af20ab3266a2d6dd849820ee0a09
Leonid <golani04@gmail.com>
2020-03-21 22:25:17 +0200

    using decorator filter unchangeable keys + decorator tests

--- backend/api/util.py
+++ backend/api/util.py
@@ -8,7 +8,7 @@ from .errors import bad_request, not_found
 
 def check_requested_data(route_func: Callable) -> Callable:
     @wraps(route_func)
-    def wrapper() -> Dict:
+    def wrapper(*args, **kwargs) -> Dict:
         data = request.get_json()
         error_msg = ""
 
@@ -20,7 +20,7 @@ def check_requested_data(route_func: Callable) -> Callable:
         if error_msg:
             return bad_request(error_msg)
 
-        return route_func(data)
+        return route_func(data, *args, **kwargs)
 
     return wrapper
 
@@ -28,14 +28,14 @@ def check_requested_data(route_func: Callable) -> Callable:
 def check_required_keys(required_keys: Set) -> Callable:
     def wrapper(route_func: Callable) -> Callable:
         @wraps(route_func)
-        def inner_wrapper(data: Dict) -> Dict:
+        def inner_wrapper(data: Dict, *args, **kwargs) -> Dict:
             if not required_keys <= set(data):
-                keys = sorted(required_keys - set(data))
+                keys: Set = sorted(required_keys - set(data))
                 return bad_request(
                     f"Missing required key{'s' if len(keys) > 1 else ''}: {', '.join(keys)}."
                 )
 
-            return route_func(data)
+            return route_func(data, *args, **kwargs)
 
         return inner_wrapper
 
@@ -51,6 +51,20 @@ def check_item_exists(model: Union[Project], error_msg: str) -> Callable:
                 return not_found(error_msg)
 
             return route_func(item, *args, **kwargs)
+
+        return inner_wrapper
+
+    return wrapper
+
+
+def filter_unchangeable_keys(unallowed_keys: Set = None) -> Callable:
+    def wrapper(route_func: Callable) -> Callable:
+        @wraps(route_func)
+        def inner_wrapper(data: Dict, project_id: str, *args, **kwargs) -> Dict:
+            if unallowed_keys is not None:
+                data = {k: v for k, v in data.items() if k not in unallowed_keys}
+
+            return route_func(project_id, data, *args, **kwargs)
 
         return inner_wrapper
 
