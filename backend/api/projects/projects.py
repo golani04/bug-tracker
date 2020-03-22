540361d562e93b1de4953567a9a4aa2733abb6c3
Leonid <golani04@gmail.com>
2020-03-22 07:57:41 +0200

    API route to modify a project

--- backend/api/projects/projects.py
+++ backend/api/projects/projects.py
@@ -3,6 +3,12 @@ from flask import jsonify
 
 from backend.api import bp
 from backend.api.errors import error_response
+from backend.api.util import (
+    check_requested_data,
+    check_required_keys,
+    check_item_exists,
+    filter_unchangeable_keys,
+)
 
 from backend.models.projects import Project
 
@@ -31,9 +37,14 @@ def get_project(project: Project):
 
 @bp.route("/projects/<string:project_id>", methods=["PUT", "PATCH"])
 @check_requested_data
+@filter_unchangeable_keys(Project.unchangeable_props)
 @check_item_exists(Project, "Required project is missing")
-def update_project(project: Project):
-    return jsonify(), 200
+def update_project(project: Project, data: Dict):
+    project = project.modify(**data)
+    if project.save() is False:
+        return error_response(500)
+
+    return jsonify(project.to_dict()), 200
 
 
 @bp.route("/projects/<string:project_id>", methods=["DELETE"])
