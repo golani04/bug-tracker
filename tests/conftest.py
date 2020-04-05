5d7a16dc30096f51bedf90c5322dc7d20a6f7009
Leonid <golani04@gmail.com>
2020-04-05 11:08:15 +0300

    Mock db issues' path

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,20 +4,24 @@ from collections import namedtuple
 from typing import NamedTuple
 
 from backend import create_app
-from tests.data import projects
+from tests.data import projects, issues
 
 
 @pytest.fixture
 def test_config() -> NamedTuple:
-    return namedtuple("TestConfig", "PROJECTS_PATH")
+    return namedtuple("TestConfig", "PROJECTS_PATH ISSUES_PATH")
 
 
 @pytest.fixture
 def tmp_config(tmpdir_factory, test_config: NamedTuple) -> NamedTuple:
+    # create propject json
     f_projects = tmpdir_factory.mktemp("projects").join("projects.json")
     f_projects.write(json.dumps(projects._data))
+    # create issue json
+    f_issues = tmpdir_factory.mktemp("issues").join("issues.json")
+    f_issues.write(json.dumps(issues._data))
 
-    return test_config(PROJECTS_PATH=f_projects)
+    return test_config(PROJECTS_PATH=f_projects, ISSUES_PATH=f_issues)
 
 
 @pytest.fixture
