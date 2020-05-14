5694c2b7ef8c983917c0fe6363f9a1996be05d28
Leonid <golani04@gmail.com>
2020-05-14 23:10:22 +0300

    api: create(post) a user + tests

--- backend/api/users/users.py
+++ backend/api/users/users.py
@@ -1,9 +1,27 @@
+from typing import Dict
 from flask import jsonify
 
 from backend.api import bp
+from backend.api.errors import error_response
+from backend.api.util import check_requested_data, check_required_keys
 from backend.models.users import User
 
 
 @bp.route("/users", methods=["GET"])
 def get_users():
     return jsonify([user.to_dict() for user in User.get_all().values()]), 200
+
+
+@bp.route("/users", methods=["POST"])
+@check_requested_data
+@check_required_keys({"name", "username", "email", "password", "project"})
+def create_user(data: Dict):
+    user = User.create(data)
+    if user.save("create") is False:
+        return error_response(500)
+
+    return jsonify(user.to_dict()), 201
+
+        return error_response(500)
+
+    return jsonify(project.to_dict()), 201
--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -1,6 +1,7 @@
 import pytest
 from backend.models.users import User
 
+_PROJECT_ID = "123456abcdefghijklmnopqrstuvwxyz" * 2
 @pytest.mark.api
 def test_users_get(app):
     response = app.get("/api/v0/users")
@@ -8,3 +9,83 @@ def test_users_get(app):
 
     json = response.get_json()
     assert set(json[0]) >= {"name", "id", "password", "username", "created", "email", "type"}
+
+
+@pytest.mark.api
+def test_create_user(app):
+    length = len(User.get_all())
+    response = app.post(
+        "/api/v0/users",
+        json={
+            "name": "Test",
+            "username": "tester",
+            "email": "tester@gmail.com",
+            "password": "password",
+            "project": _PROJECT_ID,
+            "type": 5,
+        },
+    )
+
+    assert response.status_code == 201
+
+    users = User.get_all()
+    assert length + 1 == len(users)
+
+    user_api = response.get_json()
+    assert any([True for user_id in users if user_id == user_api["id"]])
+
+
+@pytest.mark.api
+@pytest.mark.parametrize(
+    "data, expected",
+    [
+        ({"json": {}}, "Received json is empty."),
+        (
+            {
+                "data": {
+                    "name": "Test",
+                    "username": "tester",
+                    "email": "tester@gmail.com",
+                    "password": "password",
+                    "project": _PROJECT_ID,
+                    "type": 5,
+                }
+            },
+            "Provided data is not a json.",
+        ),
+        (
+            {
+                "json": {
+                    "username": "tester",
+                    "email": "tester@gmail.com",
+                    "password": "password",
+                    "project": _PROJECT_ID,
+                    "type": 5,
+                }
+            },
+            "Missing required key: name.",
+        ),
+        (
+            {
+                "json": {
+                    "name": "Test",
+                    "username": "tester",
+                    "email": "tester@gmail.com",
+                    "project": _PROJECT_ID,
+                }
+            },
+            "Missing required key: password.",
+        ),
+        (
+            {"json": {"username": "tester", "email": "tester@gmail.com",}},
+            "Missing required keys: name, password, project.",
+        ),
+    ],
+)
+def test_create_user_failed(data, expected, app, mock_model_save):
+    response = app.post("/api/v0/users", **data)
+    assert response.status_code == 400
+
+    error = response.get_json()
+    assert error["message"] == expected
+
