ad372099591686a60f118cb68a0b9c6c182bba32
Leonid <golani04@gmail.com>
2020-10-22 00:48:38 +0300

    settings file + logger

--- backend/config.py
+++ backend/config.py
@@ -1,14 +1,24 @@
 import os
-from dotenv import load_dotenv
+from pydantic import BaseSettings
+from backend.logger import init_logger
 
 
-# load env variables
-load_dotenv()
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-_db_path = os.path.normpath(os.path.join(project_path, "db_files"))
+# PROJECTS_PATH = os.path.abspath(os.path.join(_db_path, "projects", "projects.json"))
+# USERS_PATH = os.path.abspath(os.path.join(_db_path, "users", "users.json"))
+logger = init_logger(os.path.abspath(os.path.join(os.path.curdir, "logs", "back-tracker.log")))
 
 
-class db_config:
-    ISSUES_PATH = os.path.normpath(os.path.join(_db_path, "issues", "issues.json"))
-    PROJECTS_PATH = os.path.normpath(os.path.join(_db_path, "projects", "projects.json"))
-    USERS_PATH = os.path.normpath(os.path.join(_db_path, "users", "users.json"))
+class Settings(BaseSettings):
+    app_name = "Bug Tracker"
+    logger = logger
+    users_path = os.path.abspath(os.path.join(os.path.curdir, "db_files", "users", "users.json"))
+    projects_path = os.path.abspath(
+        os.path.join(os.path.curdir, "db_files", "projects", "projects.json")
+    )
+    issues_path = os.path.abspath(os.path.join(os.path.curdir, "db_files", "issues", "issues.json"))
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
