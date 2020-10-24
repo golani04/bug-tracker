08be51a47d5bd937f70aa2af0535b7df4002d3de
Leonid <golani04@gmail.com>
2020-10-24 22:40:01 +0300

    comment that explains possible structure of the db

---
+++ backend/models/__init__.py
@@ -0,0 +1,7 @@
+"""
+Organization can registerd only via registration.
+Users has fk to organization
+Issue requester fk to user
+Connect issues: Issue one to many with Users
+Project: one-to-many issues and users
+"""
