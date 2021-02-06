05a64cacf612c6938265c6bf44cdd571164dac0c
Leonid <golani04@gmail.com>
2021-02-06 17:58:27 +0200

    create user model

---
+++ backend/models/users.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy.orm import relationship
+
+from backend.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(255), unique=True, nullable=False, index=True)
+    password = Column(String(128), unique=True)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    firstname = Column(String(255), nullable=False)
+    lastname = Column(String(255), nullable=False)
+
+    created_at = Column(DateTime, default=datetime.utcnow())
+    active = Column(Boolean, default=True)
+
+    projects = relationship("Project", back_populates="user")
