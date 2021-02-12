b0fa5e2966b8e57642e236f88faee35c623a25a8
Leonid <golani04@gmail.com>
2021-02-12 19:22:44 +0200

    move hash functions to the auth utils

--- backend/schemas/util.py
+++ backend/schemas/util.py
@@ -2,8 +2,6 @@ import secrets
 from typing import Dict, List, Union
 from uuid import UUID
 
-from passlib.hash import bcrypt
-
 
 def create_id():
     return secrets.token_hex()
@@ -14,11 +12,3 @@ def find_item_by_id(items: List[Dict], item_id: Union[UUID, int]):
         item_id = str(item_id)
 
     return next((item for item in items if item["id"] == item_id), None)
-
-
-def hash_password(passw: str) -> str:
-    return bcrypt.hash(passw)
-
-
-def verify_password(passw: str, hashed: str) -> bool:
-    return bcrypt.verify(passw, hashed)
