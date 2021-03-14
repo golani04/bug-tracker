3c1e73c961af579607f8034d49cb468b2738b54c
Leonid <golani04@gmail.com>
2021-03-14 23:16:28 +0200

    class method create user

--- backend/models/users.py
+++ backend/models/users.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from sqlalchemy import Boolean, Column, DateTime, Integer, SmallInteger, String
 from sqlalchemy.orm import relationship
 
 from backend.db import Base
+from backend.schemas.users import UserType
+from backend.utils.auth import hash_password
 
 
 class User(Base):
@@ -15,8 +17,20 @@ class User(Base):
     email = Column(String(255), unique=True, nullable=False, index=True)
     firstname = Column(String(255), nullable=False)
     lastname = Column(String(255), nullable=False)
+    role = Column(SmallInteger, nullable=False, default=int(UserType.viewer))
 
     created_at = Column(DateTime, default=datetime.utcnow())
     active = Column(Boolean, default=True)
 
-    projects = relationship("Project", back_populates="user")
+    projects = relationship("Project", back_populates="owner")
+
+    @classmethod
+    def create_user(cls, username: str, password: str, fname: str, lname: str, role: int) -> "User":
+        return cls(
+            username=username,
+            email=username,
+            password=hash_password(password),
+            firstname=fname,
+            lastname=lname,
+            role=role,
+        )
