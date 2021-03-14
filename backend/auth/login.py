a3b700e63f1ccd86fa7603d89b884f22628833ec
Leonid <golani04@gmail.com>
2021-03-14 23:04:28 +0200

    authentication routes

--- backend/auth/login.py
+++ backend/auth/login.py
@@ -1,31 +1,48 @@
-from dataclasses import dataclass
+import logging
 
-import pydantic
-from fastapi import Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel  # pylint: disable=no-name-in-module
 from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
-from backend.auth import router
+from backend.utils.auth import hash_password, verify_password
 from backend.db import get_db
 from backend.models.users import User
+from backend.schemas.users import UserType
 
 
-@dataclass
-class LoggedInUser:
+router = APIRouter()
+logger = logging.getLogger("bug_tracker")
+
+
+class LoggedInUser(BaseModel):
     username: str
     password: str
 
 
-LoggedInUser = pydantic.dataclasses.dataclass(LoggedInUser)
+@router.post("/login", status_code=status.HTTP_200_OK)
+async def login(user: LoggedInUser, session: Session = Depends(get_db)):
+    current_user: User = (
+        session.query(User)
+        .filter(or_(User.email == user.username, User.username == user.username))
+        .one_or_none()
+    )
 
+    if current_user is None and not verify_password(user.password, hash_password("timingattack")):
+        logger.info(f"{user.username} is missing.")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or password is incorrect."
+        )
 
-@router.post("/login", status_code=status.HTTP_204_NO_CONTENT)
-def login(user: LoggedInUser, session: Session = Depends(get_db)):
-    user = session.query.filter(
-        or_(User.email == user.username, User.username == user.username)
-    ).one_or_none()
+    if not verify_password(user.password, current_user.password):
+        logger.info(f"{user.username} entered wrong password.")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or password is incorrect."
+        )
 
-    if user is None:
-        raise HTTPException(404, detail="User is missing")
-
-    return {}
+    return {
+        "success": True,
+        "is_logged_in": True,
+        "username": current_user.username,
+        "role": UserType(current_user.role).name,
+    }
