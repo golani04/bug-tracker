4d58ad699b7f057aa9d7d7c659ee53fe3ec13172
Leonid <golani04@gmail.com>
2021-03-27 21:28:24 +0300

    comment out api routes

--- backend/api/issues.py
+++ backend/api/issues.py
@@ -1,5 +1,4 @@
 from backend.schemas.util import find_item_by_id
-from backend.db import FileDatabase
 from fastapi import APIRouter, status, HTTPException
 from typing import List
 
@@ -7,28 +6,25 @@ from backend.schemas.issues import Issue, IssueCreate
 
 
 router = APIRouter()
-db = FileDatabase()
 
 
-@router.get("/", response_model=List[Issue])
-def get_issues():
-    return db.get_issues()
+# @router.get("/", response_model=List[Issue])
+# def get_issues():
+#     return db.get_issues()
 
 
-@router.post("/", response_model=Issue, status_code=status.HTTP_201_CREATED)
-def create_issue(data: IssueCreate):
-    issue = IssueCreate(data)
-    return issue
+# @router.post("/", response_model=Issue, status_code=status.HTTP_201_CREATED)
+# def create_issue(data: IssueCreate):
+#     issue = IssueCreate(data)
+#     return issue
 
 
-@router.get("/{issue_id}", response_model=Issue)
-def get_issue(issue_id: int):
-    issue = find_item_by_id(db.get_issues(), issue_id)
-    if issue is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Required issue is missing"
-        )
+# @router.get("/{issue_id}", response_model=Issue)
+# def get_issue(issue_id: int):
+#     issue = find_item_by_id(db.get_issues(), issue_id)
+#     if issue is None:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail="Required issue is missing"
+#         )
 
-    return issue
-
-
+#     return issue
--- backend/api/projects.py
+++ backend/api/projects.py
@@ -3,25 +3,23 @@ from typing import List
 from uuid import UUID
 from fastapi import APIRouter, HTTPException, status
 
-from backend.db import FileDatabase
 from backend.schemas.projects import Project
 
 
 router = APIRouter()
-db = FileDatabase()
 
 
-@router.get("/", response_model=List[Project])
-def get_projects():
-    return db.get_projects()
+# @router.get("/", response_model=List[Project])
+# def get_projects():
+#     return db.get_projects()
 
 
-@router.get("/{project_id}", response_model=Project)
-def get_issue(project_id: UUID):
-    project = find_item_by_id(db.get_projects(), project_id)
-    if project is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Required project is missing"
-        )
+# @router.get("/{project_id}", response_model=Project)
+# def get_issue(project_id: UUID):
+#     project = find_item_by_id(db.get_projects(), project_id)
+#     if project is None:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail="Required project is missing"
+#         )
 
-    return project
+#     return project
