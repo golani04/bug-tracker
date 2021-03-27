594dce3108260f761ba5ea2367134f0a5736d828
Leonid <golani04@gmail.com>
2021-03-27 15:01:32 +0300

    auth routes

--- backend/auth/login.py
+++ backend/auth/login.py
@@ -1,11 +1,10 @@
 import logging
 
-from fastapi import APIRouter, Depends, HTTPException, status
-from pydantic import BaseModel  # pylint: disable=no-name-in-module
+import pydantic
+from fastapi import APIRouter, Body, Depends, HTTPException, Response, status
 from sqlalchemy import or_
 from sqlalchemy.orm import Session
 
-from backend.utils.auth import hash_password, verify_password
 from backend.db import get_db
 from backend.models.users import User
 from backend.schemas.users import UserType
@@ -14,35 +13,22 @@ from backend.schemas.users import UserType
 router = APIRouter()
 logger = logging.getLogger("bug_tracker")
 
+LoggedInUser = pydantic.dataclasses.dataclass(LoggedInUser)
+router = APIRouter()
 
 class LoggedInUser(BaseModel):
     username: str
     password: str
 
-
-@router.post("/login", status_code=status.HTTP_200_OK)
-async def login(user: LoggedInUser, session: Session = Depends(get_db)):
-    current_user: User = (
+@router.post("/login", status_code=status.HTTP_204_NO_CONTENT)
+def login(user: LoggedInUser = Body(...), session: Session = Depends(get_db)):
+    user = (
         session.query(User)
         .filter(or_(User.email == user.username, User.username == user.username))
         .one_or_none()
     )
 
-    if current_user is None and not verify_password(user.password, hash_password("timingattack")):
-        logger.info(f"{user.username} is missing.")
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or password is incorrect."
-        )
+    if user is None:
+        raise HTTPException(404, detail="User is missing")
 
-    if not verify_password(user.password, current_user.password):
-        logger.info(f"{user.username} entered wrong password.")
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Username or password is incorrect."
-        )
-
-    return {
-        "success": True,
-        "is_logged_in": True,
-        "username": current_user.username,
-        "role": UserType(current_user.role).name,
-    }
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
