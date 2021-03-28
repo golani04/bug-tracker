34bb4a9645466bb5a797359c176cfcb46af03fcb
Leonid <golani04@gmail.com>
2021-03-28 23:58:32 +0300

    set app logger

--- main.py
+++ main.py
@@ -11,16 +11,10 @@ from logger import init_logger
 
 app = FastAPI(title="Bug Tracker")
 app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+# create logger
+init_logger(os.path.abspath(os.path.join(os.path.curdir, "logs", "bug_tracker.log")))
 
 # define routers
 app.include_router(routers, prefix="/api/v1")
 app.include_router(auth_routers, prefix="/auth")
 app.include_router(main_router)
-
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception(request: Request, exc: RequestValidationError) -> JSONResponse:
-    return await JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
-    )
