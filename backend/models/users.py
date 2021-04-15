43d2be2e84e984722651fe412c05e2dc2e45c30a
Leonid <golani04@gmail.com>
2021-04-15 14:42:21 +0300

    models that represent DB tables

--- backend/models/users.py
+++ backend/models/users.py
@@ -13,11 +13,25 @@ class User(Base):
 
     id = Column(Integer, primary_key=True)
     username = Column(String(255), unique=True, nullable=False, index=True)
-    password = Column(String(128), unique=True)
+    password = Column(String(128), nullable=True)
     email = Column(String(255), unique=True, nullable=False, index=True)
     firstname = Column(String(255), nullable=False)
     lastname = Column(String(255), nullable=False)
     role = Column(SmallInteger, nullable=False, default=int(UserType.viewer))
 
     created_at = Column(DateTime, default=datetime.utcnow())
+    updated_at = Column(DateTime, onupdate=datetime.utcnow())
     active = Column(Boolean, default=True)
+
+    @classmethod
+    def create_user(
+        cls, email, username, password, fname, lname: str, role: int = int(UserType.viewer)
+    ):
+        return cls(
+            email=email,
+            username=username,
+            password=hash_password(password),
+            firstname=fname,
+            lastname=lname,
+            role=role,
+        )
