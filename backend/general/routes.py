0536d99dbf2cb24837dcff7fcccd4b0e0d50a731
Leonid <golani04@gmail.com>
2021-04-16 20:50:33 +0300

    send issue data using general template route

--- backend/general/routes.py
+++ backend/general/routes.py
@@ -1,20 +1,39 @@
 import logging
+from datetime import date
+from typing import List
 
-from fastapi import APIRouter, Request
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import FileResponse
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.orm import Session
+
+from backend.db import get_db
+from backend.models.issues import Issue as IssueTable
+from backend.schemas.issues import Issue as IssueSchema
+from backend.schemas.projects import Project as ProjectSchema
+from backend.schemas.users import User as UserSchema
+from backend.utils.html import templates
 
 
 router = APIRouter()
 logger = logging.getLogger("bug_tracker")
-templates = Jinja2Templates(directory="frontend/components")
 
 
 @router.get("/")
 @router.get("/{template}")
-def index(request: Request, template: str = None):
+def index(request: Request, template: str = None, session: Session = Depends(get_db)):
     if template is None:
         return templates.TemplateResponse("index.html", {"request": request})
 
+    data = []
+    if template.startswith("issues"):
+        data: List[IssueTable] = session.query(IssueTable).all()
+        data = [
+            {
+                **IssueSchema.from_orm(item).dict(),
+                "project": ProjectSchema.from_orm(item.project),
+                "user": UserSchema.from_orm(item.owner),
+            }
+            for item in data
+        ]
+
     template = template if template.endswith(".html") else f"{template}.html"
-    return templates.TemplateResponse(f"pages/{template}", {"request": request})
+    return templates.TemplateResponse(f"pages/{template}", {"request": request, "data": data})
