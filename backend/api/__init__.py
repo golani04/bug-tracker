17ce06c956e0c7643c77e1217931a488c5bdb0c0
Leonid <golani04@gmail.com>
2021-04-24 00:28:36 +0300

    support display/update of the current user

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 from backend.api.issues import router as issue_router
-from backend.api.projects import router as project_router
+from backend.api.users import router as user_router
 
 
 routers = APIRouter()
 
 routers.include_router(issue_router, tags=["Issues"], prefix="/issues")
-routers.include_router(project_router, tags=["Projects"], prefix="/projects")
+routers.include_router(user_router, tags=["Users"], prefix="/users")
---
+++ backend/api/users.py
@@ -0,0 +1,29 @@
+from urllib.parse import urljoin
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, Request, status
+from fastapi.responses import RedirectResponse
+from sqlalchemy import update
+from sqlalchemy.orm import session
+
+from backend.db import get_db
+from backend.models.users import User as UserTable
+from backend.schemas.users import UserUpdate
+
+
+router = APIRouter()
+
+
+@router.post("/{user_id}")
+async def get_issue(request: Request, user_id: int, session: session = Depends(get_db)):
+    data = await request.form()
+    session.execute(
+        update(UserTable)
+        .where(UserTable.id == user_id)
+        .values(**UserUpdate(**data).dict(exclude_unset=True))
+    )
+    session.commit()
+
+    return RedirectResponse(
+        urljoin(str(request.base_url), "user"), status_code=status.HTTP_303_SEE_OTHER
+    )
