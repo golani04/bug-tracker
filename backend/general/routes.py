f58088d11bb5400337844fa6ea506063ee60b767
Leonid <golani04@gmail.com>
2021-04-23 19:04:10 +0300

    pass item_id  in query params, to set current_issue

--- backend/general/routes.py
+++ backend/general/routes.py
@@ -2,7 +2,7 @@ import logging
 from datetime import date
 from typing import List
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Query, Request
 from sqlalchemy.orm import Session
 
 from backend.db import get_db
@@ -19,11 +19,17 @@ logger = logging.getLogger("bug_tracker")
 
 @router.get("/")
 @router.get("/{template}")
-def index(request: Request, template: str = None, session: Session = Depends(get_db)):
+def index(
+    request: Request,
+    template: str = None,
+    item_id: int = Query(None),
+    session: Session = Depends(get_db),
+):
     if template is None:
-        return templates.TemplateResponse("index.html", {"request": request})
+        return templates.TemplateResponse("index.html", {"request": request, "current_issue": {}})
 
     data = []
+    current_issue = {}
     if template.startswith("issues"):
         data: List[IssueTable] = session.query(IssueTable).all()
         data = [
@@ -34,6 +40,10 @@ def index(request: Request, template: str = None, session: Session = Depends(get
             }
             for item in data
         ]
+        if item_id is not None:
+            current_issue = next((issue for issue in data if issue["id"] == item_id), {})
 
     template = template if template.endswith(".html") else f"{template}.html"
-    return templates.TemplateResponse(f"pages/{template}", {"request": request, "data": data})
+    return templates.TemplateResponse(
+        f"pages/{template}", {"request": request, "data": data, "current_issue": current_issue}
+    )
