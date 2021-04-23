207467e0e06e0bb56e4df14d46fa6c354b1f5a64
Leonid <golani04@gmail.com>
2021-04-24 00:37:54 +0300

    get issue data from separate function, rename variable

--- backend/general/routes.py
+++ backend/general/routes.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 
 from backend.db import get_db
 from backend.models.issues import Issue as IssueTable
+from backend.models.users import User as UserTable
 from backend.schemas.issues import Issue as IssueSchema
 from backend.schemas.projects import Project as ProjectSchema
 from backend.schemas.users import User as UserSchema
@@ -15,6 +16,19 @@ from backend.utils.html import templates
 
 router = APIRouter()
 logger = logging.getLogger("bug_tracker")
+
+
+def get_issues_data(data: List[IssueTable], item_id: int):
+    data = [
+        {
+            **IssueSchema.from_orm(item).dict(),
+            "project": ProjectSchema.from_orm(item.project),
+            "user": UserSchema.from_orm(item.owner),
+        }
+        for item in data
+    ]
+
+    return data, next((issue for issue in data if issue["id"] == item_id), {})
 
 
 @router.get("/")
@@ -26,24 +40,18 @@ def index(
     session: Session = Depends(get_db),
 ):
     if template is None:
-        return templates.TemplateResponse("index.html", {"request": request, "current_issue": {}})
+        return templates.TemplateResponse("index.html", {"request": request, "current_item": {}})
 
     data = []
-    current_issue = {}
+    current_item = {}
     if template.startswith("issues"):
         data: List[IssueTable] = session.query(IssueTable).all()
-        data = [
-            {
-                **IssueSchema.from_orm(item).dict(),
-                "project": ProjectSchema.from_orm(item.project),
-                "user": UserSchema.from_orm(item.owner),
-            }
-            for item in data
-        ]
-        if item_id is not None:
-            current_issue = next((issue for issue in data if issue["id"] == item_id), {})
+        data, current_item = get_issues_data(data, item_id)
+    elif template.startswith("user"):
+        user: List[IssueTable] = session.query(UserTable).one_or_none()
+        current_item = UserSchema.from_orm(user).dict()
 
     template = template if template.endswith(".html") else f"{template}.html"
     return templates.TemplateResponse(
-        f"pages/{template}", {"request": request, "data": data, "current_issue": current_issue}
+        f"pages/{template}", {"request": request, "data": data, "current_item": current_item}
     )
