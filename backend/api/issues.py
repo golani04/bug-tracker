d7eef8641fdb5af2e492a8cff873e2c0388316ea
Leonid <golani04@gmail.com>
2021-05-15 19:38:50 +0300

    some latest updates to the html

--- backend/api/issues.py
+++ backend/api/issues.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Session
 from backend.db import get_db
 from backend.models.issues import Issue as IssueTable
 from backend.schemas.issues import Issue as IssueSchema
-from backend.schemas.issues import IssueCreate
+from backend.schemas.issues import IssueCreate, IssueDetails, Label, Severity, Status
 
 
 router = APIRouter()
@@ -18,7 +18,7 @@ router = APIRouter()
 @router.get("/", response_model=List[IssueSchema])
 def get_issues(session: Session = Depends(get_db)):
     issues: List[Tuple[IssueTable]] = session.execute(select(IssueTable)).all()
-    return [issue for (issue,) in issues]
+    return [IssueSchema.from_orm(issue).dict() for (issue,) in issues]
 
 
 @router.post("/")
@@ -40,3 +40,16 @@ async def create_issue(request: Request, session: Session = Depends(get_db)):
     return RedirectResponse(
         urljoin(str(request.base_url), "issues"), status_code=status.HTTP_303_SEE_OTHER
     )
+
+
+@router.get(
+    "/details", status_code=status.HTTP_200_OK, tags=["Issues"], response_model=IssueDetails
+)
+def get_issue_details():
+    return {
+        "label": {int(label): str(label).replace("_", " ").title() for label in Label},
+        "status": {int(status): str(status).replace("_", " ").title() for status in Status},
+        "severity": {
+            int(severity): str(severity).replace("_", " ").title() for severity in Severity
+        },
+    }
