ec41ad5ea6dfcf6cf5288672d260ed82f4629df2
Leonid <golani04@gmail.com>
2022-05-21 15:05:31 +0300

    add stram handler to the logs

--- logger.py
+++ logger.py
@@ -1,16 +1,22 @@
 import logging
-import os
+import sys
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
+from typing import Union
 
 
-def init_logger(log_path: Path = None):
-    if log_path is None:
-        log_path = os.path.abspath(os.path.join(os.path.curdir, "logs", "bug_tracker.log"))
+def init_logger(_log_path: Path | None = None) -> None:
+    log_path: Union[str, Path] = _log_path or Path().joinpath("logs", "bug_tracker.log").resolve()
+
     fmt = logging.Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]")
     fh = RotatingFileHandler(log_path, maxBytes=10240, backupCount=10)
     fh.setFormatter(fmt)
 
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setFormatter(fmt)
+
     logger = logging.getLogger("bug_tracker")
+    # set handlers for the current logger
     logger.addHandler(fh)
+    logger.addHandler(ch)
     logger.setLevel(logging.DEBUG)
