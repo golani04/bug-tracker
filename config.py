30f1e42a96df0d48106419c3927bc53dba778b25
Leonid <golani04@gmail.com>
2022-05-28 22:45:55 +0300

    add auth props to config

--- config.py
+++ config.py
@@ -1,15 +1,22 @@
 from functools import lru_cache
+from typing import Tuple
 
+from jose.constants import Algorithms
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    app_name: str = "Bug Tracker"
     sqlalchemy_database_url: str
     debug: bool
     admin_user: str
     admin_pass: str
     admin_email: str
-    app_name: str = "Bug Tracker"
+    AUTH_HEADERS: Tuple[str] = ("cookie",)
+    COOKIE_HEADER_NAME: str = "access_token_cookie"
+    SECRET_KEY: str
+    ALGORITHM: str = Algorithms.HS256
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 180
 
     class Config:
         env_file = ".env"
