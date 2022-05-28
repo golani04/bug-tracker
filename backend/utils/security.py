be33770f2e6c36a88fc8c75f3b9d31aa04d46dcf
Leonid <golani04@gmail.com>
2022-05-28 23:20:52 +0300

    authentication and security handlers

---
+++ backend/utils/security.py
@@ -0,0 +1,9 @@
+from passlib.hash import bcrypt_sha256
+
+
+def hash_password(passw: str) -> str:
+    return bcrypt_sha256.hash(passw)
+
+
+def verify_password(passw: str, hashed: bytes) -> bool:
+    return bcrypt_sha256.verify(passw, hashed)
