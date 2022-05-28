380c68428d5e993d3b9cef2f49b509034accdbbb
Leonid <golani04@gmail.com>
2022-05-28 23:22:51 +0300

    if auth exception raised redirect to login page

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-import os
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
 from backend.api import routers
 from backend.auth import auth_routers
 from backend.general import main_router
+from backend.utils.exceptions import AuthError
 from logger import init_logger
 
 
 app = FastAPI(title="Bug Tracker")
 app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+
+@app.exception_handler(AuthError)
+async def auth_error_handler(request: Request, exc: AuthError) -> RedirectResponse:
+    return RedirectResponse("/auth/login", status_code=status.HTTP_307_TEMPORARY_REDIRECT)
+
+
 # create logger
 init_logger()
 
